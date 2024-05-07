6313dca3a86a8c77865d8497eb77e764f31f8365
xdjeremy <jeremy.fran@icloud.com>
2024-05-07 15:26:46 +0000

    add login form design (#11)
    ### TL;DR
    
    This PR enhances the login page UI and adds functionality for user authentication.
    
    ### What changed?
    
    Prettier configuration is added to handle code formatting. Login or authentication form in the application is revised to improve the user interface. It includes the addition of existing UI components like `Form`, `FormControl`, `FormField`, `FormItem`, `Input`, `Lock`, `User`, `Button`, and `Logo` to the login form module. It also includes changes to the `LoginForm` and `LoginPage` component.
    
    ### How to test?
    
    Pull this PR to your local environment, navigate to the login page, and try to login. You should now see enhanced UI with logo, user email, and password fields.
    
    ### Why make this change?
    
    The motivation for these changes is to improve the user interface and the overall user experience on the login page and to add the ability for users to authenticate themselves.
    
    ---

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -1,15 +1,58 @@
+'use client'
 import React from 'react'
-import { Form } from '@/components/ui/form'
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import LoginSchema from '@/app/(auth)/login/login-schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from '@/components/ui/input'
+import { Lock, User } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 const LoginForm = () => {
-  const form = useForm<z.infer<typeof LoginSchema>>()
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: { email: '', password: '' },
+  })
 
   return (
     <Form {...form}>
-      <form></form>
+      <form className={'mx-auto w-full max-w-xs space-y-3'}>
+        <FormField
+          control={form.control}
+          name={'email'}
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  {...field}
+                  icon={<User />}
+                  placeholder={'email@sdsolutions.com'}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={'password'}
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  {...field}
+                  icon={<Lock />}
+                  type={'password'}
+                  placeholder={'mysecretpassword'}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type={'submit'} className={'w-full'}>
+          Login
+        </Button>
+      </form>
     </Form>
   )
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import LoginForm from '@/app/(auth)/login/login-form'
+import LogoFull from '@/assets/images/Logo Full.png'
+import Image from 'next/image'
 
 const LoginPage = () => {
-  return <div></div>
+  return (
+    <div
+      className={
+        'gap-5g flex min-h-screen w-full flex-col items-center justify-center'
+      }
+    >
+      <Image src={LogoFull} alt={'SD Solutions'} width={320} />
+      <LoginForm />
+    </div>
+  )
 }
 
 export default LoginPage
