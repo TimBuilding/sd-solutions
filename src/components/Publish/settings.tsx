a052184a18a115290576e83eb0402b48d9130568
xdjeremy <jeremy.fran@icloud.com>
2024-05-07 16:02:01 +0000

    feat(feed): add option picker on publish form (#27)

--- src/components/Publish/settings.tsx
+++ src/components/Publish/settings.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { Bell, Megaphone } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 const Settings = () => {
   return (
     <div className={'bg-muted'}>
-      <div></div>
+      <RadioGroup className={'gap-0 px-2'} defaultValue={'activity-feed'}>
+        <div className={'flex flex-row items-center justify-between p-1.5'}>
+          <div className={'flex flex-row items-center justify-center gap-2.5'}>
+            <RadioGroupItem value={'activity-feed'} />
+            <Button
+              variant={'outline'}
+              size={'icon'}
+              className={'rounded-full'}
+            >
+              <Bell className={'h-5 w-5 text-muted-foreground/30'} />
+            </Button>
+            <span className={'text-sm font-medium'}>Activity Feed</span>
+          </div>
+        </div>
+        <div className={'flex flex-row items-center justify-between p-1.5'}>
+          <div className={'flex flex-row items-center justify-center gap-2.5'}>
+            <RadioGroupItem value={'announcements'} />
+            <Button
+              variant={'outline'}
+              size={'icon'}
+              className={'rounded-full'}
+            >
+              <Megaphone className={'h-5 w-5 text-muted-foreground/30'} />
+            </Button>
+            <span className={'text-sm font-medium'}>Announcements</span>
+          </div>
+        </div>
+      </RadioGroup>
     </div>
   )
 }
