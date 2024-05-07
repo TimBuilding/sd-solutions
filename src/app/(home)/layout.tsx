0a36d02b3343a328775f241ee730dc1e59d2dbd4
tamrivera05 <tamangelarivera@gmail.com>
2024-05-07 23:06:55 +0000

    TIM-94 feat:logo for layout (#31)

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { FC, ReactNode } from 'react'
 import Navbar from '@/components/layout/Navbar'
 
 interface Props {
-  children: React.ReactNode
+  children: ReactNode
 }
-const Layout: React.FC<Props> = ({ children }) => {
+
+const Layout: FC<Props> = ({ children }) => {
   return (
-    <div className="flex w-full flex-row items-center justify-between bg-white px-4 py-2 drop-shadow-md">
+    <div className="flex w-full flex-col items-center justify-between bg-white px-4 py-2 drop-shadow-md ">
       <Navbar />
     </div>
   )
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,12 +5,19 @@ import { Search } from 'lucide-react'
 import { Menu } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { useState } from 'react'
+import SDSolutionsLogoMinimal from '@/components/SDSolutionsLogo-Minimal'
 
 const Navbar = () => {
   return (
     <div className="flex w-full flex-row justify-between">
-      <div className="flex flex-row justify-start gap-2">
+      <div className="flex flex-row items-center justify-start gap-2.5">
+        <SDSolutionsLogoMinimal
+          className="h-6 w-6 bg-transparent fill-black"
+          style={{
+            filter:
+              'invert(17%) sepia(17%) saturate(7489%) hue-rotate(158deg) brightness(92%) contrast(89%)',
+          }}
+        />
         <Button variant="ghost" className="group hover:bg-red-500">
           <Heart className="h-4 w-4 text-[#999999] group-hover:animate-pulse group-hover:fill-white group-hover:text-white group-hover:delay-0 group-hover:duration-1000" />
         </Button>
