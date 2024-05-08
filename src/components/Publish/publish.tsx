0f48292593139bc70bc8b40072445a2574997789
xdjeremy <jeremy.fran@icloud.com>
2024-05-08 05:37:54 +0000

    TIM-97 feat(feed): add exit button on text area publish (#32)

--- src/components/Publish/publish.tsx
+++ src/components/Publish/publish.tsx
@@ -6,12 +6,23 @@ import Activity from '@/components/Publish/activity'
 import { Separator } from '@/components/ui/separator'
 import Settings from '@/components/Publish/settings'
 import { Button } from '@/components/ui/button'
+import { X } from 'lucide-react'
 
 const Publish = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <Card className={'w-full max-w-3xl pt-10'}>
+    <Card className={'relative w-full max-w-3xl pt-10'}>
+      {isOpen && (
+        <Button
+          variant={'ghost'}
+          size={'icon'}
+          className={'absolute right-2 top-2 h-6 w-6'}
+          onClick={() => setIsOpen(false)}
+        >
+          <X className={'h-4 w-4'} />
+        </Button>
+      )}
       <PublishInput setIsOpen={setIsOpen} />
       {isOpen && (
         <>
