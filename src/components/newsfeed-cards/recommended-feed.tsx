35ef6b1a62e400e0ccc896290411dd017bcc7a87
xdjeremy <jeremy.fran@icloud.com>
2024-05-09 09:18:24 +0000

    TIM-123 feat(feed): add recommended feed card links (#69)

--- src/components/newsfeed-cards/recommended-feed.tsx
+++ src/components/newsfeed-cards/recommended-feed.tsx
@@ -1,33 +1,79 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import { Bell, ChevronRight } from 'lucide-react'
+import {
+  Bell,
+  Calendar,
+  ChevronRight,
+  LucideIcon,
+  Megaphone,
+} from 'lucide-react'
 import { Separator } from '@/components/ui/separator'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
-const Feed = () => {
+interface Feed {
+  title: string
+  description: string
+  link: string
+  icon: LucideIcon
+}
+
+const links: Feed[] = [
+  {
+    title: 'News Feed',
+    description: 'Latest updates',
+    link: '/',
+    icon: Bell,
+  },
+  {
+    title: 'Announcements',
+    description: 'Company news',
+    link: '/announcements',
+    icon: Megaphone,
+  },
+  {
+    title: 'Events',
+    description: 'Upcoming events',
+    link: '/events',
+    icon: Calendar,
+  },
+]
+
+const Feed: FC<Feed> = ({ title, description, link, icon: Icon }) => {
   return (
-    <div className={'flex flex-row justify-between gap-8 py-4'}>
+    <Link
+      href={link}
+      className={
+        'flex w-full flex-row justify-between gap-8 rounded-lg px-6 py-5 hover:bg-muted/50'
+      }
+    >
       <div className={'flex flex-row items-center justify-center'}>
         <span
           className={
             'flex h-10 w-10 flex-row items-center justify-center rounded-full bg-primary'
           }
         >
-          <Bell />
+          <Icon />
         </span>
         <div className={'flex flex-col items-start justify-center px-2.5'}>
-          <span className={'text-sm font-semibold text-card-foreground'}>
-            News Feed
+          <span
+            className={'text-sm font-semibold capitalize text-card-foreground'}
+          >
+            {title}
           </span>
           <span className={'text-xs text-card-foreground/30'}>
-            Latest updates
+            {description}
           </span>
         </div>
       </div>
-      <Button variant={'ghost'} size={'icon'}>
+      <Button
+        variant={'ghost'}
+        size={'icon'}
+        className={'hover:bg-transparent'}
+      >
         <ChevronRight />
       </Button>
-    </div>
+    </Link>
   )
 }
 
@@ -38,10 +84,16 @@ const RecommendedFeed = () => {
         Recommended Feeds
       </CardHeader>
       <Separator />
-      <CardContent>
-        <Feed />
-        <Feed />
-        <Feed />
+      <CardContent className={'flex flex-col px-0'}>
+        {links.map((link) => (
+          <Feed
+            key={link.title}
+            title={link.title}
+            description={link.description}
+            link={link.link}
+            icon={link.icon}
+          />
+        ))}
       </CardContent>
     </Card>
   )
