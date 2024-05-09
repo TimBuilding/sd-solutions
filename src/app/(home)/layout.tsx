a49db18f5fe88af7fd7a0a5f76ec75ffa08dc4e0
xdjeremy <jeremy.fran@icloud.com>
2024-05-09 09:18:19 +0000

    TIM-107 feat(auth): redirect when user is not logged in (#67)

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,25 @@
+'use server'
 import React, { FC, ReactNode } from 'react'
 import Navbar from '@/components/layout/Navbar'
+import { createServerClient } from '@/utils/supabase'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 interface Props {
   children: ReactNode
 }
 
-const Layout: FC<Props> = ({ children }) => {
+const Layout: FC<Props> = async ({ children }) => {
+  const supabase = createServerClient(cookies())
+
+  // check if user is not logged in
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+  if (!user) {
+    redirect('/login')
+  }
+
   return (
     <div className={'flex w-full flex-col'}>
       <Navbar />
